aa17780734d1ac650e8fcac1c627c08b0630e7ba
iamdevlab <247cashsystem@gmail.com>
2025-11-03 10:47:08 +0100

    updated CORS values

--- app/main.py
+++ app/main.py
@@ -25,6 +25,8 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://www.eduforgeplanner.com",
+    "https://eduforgeplanner.com",
 ]
 app.add_middleware(
     CORSMiddleware,
